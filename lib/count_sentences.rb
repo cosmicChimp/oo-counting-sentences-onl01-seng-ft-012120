c7798a759c272d1ba810a57fa43ddc2a56f6daa1
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-31 19:34:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!+]/)#reject {|string| string.empty?}
+    new_array = self.split(/[.?!+]/).reject {|string| string.empty?}
     print new_array
   end
 end
